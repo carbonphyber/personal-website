74010a0eaeb2a8af27cff30626aa38b6274da75b
David Wortham <djwortham@gmail.com>
2021-03-21 21:57:17 -0700

    fix(*): improved CSS styles for Rescue Fosters page

--- src/DogsRescueFostersPage.jsx
+++ src/DogsRescueFostersPage.jsx
@@ -96,7 +96,7 @@ const dogsArray = [
     dateRescued: '2015-05-26',
     dateAdopted: '2015-05-30',
     imageUrl: '/i/coppersdream.org/rachel/DSC_0599.JPG',
-    captionAlign: 'right',
+    captionAlign: 'left',
   },
   {
     name: 'Speckles',
@@ -254,7 +254,11 @@ const dogsArray = [
     imageUrl: '',
     captionAlign: 'left',
   },
-];
+].map((dog) => ({
+  ...dog,
+  id: dog.name.toLowerCase().replace(/[^a-z]+/, '_').replace(/^\s+|\s+$/g, ''),
+  description: `${dog.name}, the ${dog.ageGuess}; ${dog.weight} ${dog.breedGuess}`,
+}));
 
 dogsArray.sort((a, b) => {
   if (a.dateRescued < b.dateRescued) return 1;
@@ -278,32 +282,32 @@ export function DogsRescueFostersPage() {
       </header>
 
       <header>
-        <p class="lead">David fosters adoptable dogs for <a href="https://coppersdream.org/" className="coppers-dream-colors" rel="noopener noreferrer">CoppersDream.org</a>, <a href="http://dpsrescue.com/" className="doggie-protective-service-colors" rel="noopener noreferrer">Doggie Protective Services (DPS)</a>, and <a href="https://ilovefamilydog.org/" className="family-dog-rescue-colors" rel="noopener noreferrer">Family Dog Rescue</a>, animal rescues in the San Francisco Bay Area.</p>
+        <p className="lead">David fosters adoptable dogs for <a href="https://coppersdream.org/" className="coppers-dream-colors" rel="noopener noreferrer">CoppersDream.org</a>, <a href="http://dpsrescue.com/" className="doggie-protective-service-colors" rel="noopener noreferrer">Doggie Protective Services (DPS)</a>, and <a href="https://ilovefamilydog.org/" className="family-dog-rescue-colors" rel="noopener noreferrer">Family Dog Rescue</a>, animal rescues in the San Francisco Bay Area.</p>
         <p>These rescue groups pull dogs from high risk animal shelters in Central California, delivers them to fosters, and the fosters take care of them while they wait to be adopted. Family Dog Rescue tends to pull more &quot;special needs&quot; dogs (including blind, deaf, both, skin infections, medical conditions, etc), northern breeds (Husky, Malmute, and wolf hybrids) and also pulls dogs from other countries including Mexico and Burma.</p>
         <p>David enjoys photographing and training his foster dogs.</p>
       </header>
 
       {dogsArray.map((dog) => (
-        <div class="row">
+        <div className="row">
           <figure
-            class={`jumbotron rescue-dog rescue-dog-${dog.id} ${dog.imageUrl ? '' : 'no-image'}`}
+            className={`jumbotron rescue-dog rescue-dog-${dog.id} ${dog.imageUrl ? '' : 'no-image'}`}
             id={dog.id}
             style={{
               backgroundImage: `url(${dog.imageUrl})` || '',
             }}
           >
-            <figcaption class={`rescue-dog--caption rescue-dog--caption__${dog.captionAlign || 'left'}`}>
-              <h2><a href={`#${dog.id}`} className="rescue-dog--header">{dog.name}</a></h2>
-              <h3 class="rescue-dog--breed">{dog.breedGuess || ''}</h3>
-              <h3 class="rescue-dog--vitals">{[dog.ageGuess, dog.weight].join(', ')}</h3>
-              <p>
-                <span class="label label-info">{`Rescued ${dog.dateRescued}`}</span>
-                &nbsp;
-                {dog.dateAdopted && <span class="label label-success">{`Adopted ${dog.dateAdopted}`}</span>}
+            <figcaption className={`rescue-dog--caption rescue-dog--caption__${dog.captionAlign || 'left'}`}>
+              <h2 className="rescue-dog--header"><a href={`#${dog.id}`}>{dog.name}</a></h2>
+              <h3 className="rescue-dog--breed">{dog.breedGuess || ''}</h3>
+              <h3 className="rescue-dog--vitals">{[dog.ageGuess, dog.weight].join(', ')}</h3>
+              <p className="rescue-dog--labels">
+                <span className="label label-info">{`Rescued ${dog.dateRescued}`}</span>
+                <br />
+                {dog.dateAdopted && <span className="label label-success">{`Adopted ${dog.dateAdopted}`}</span>}
               </p>
             </figcaption>
-            <div class="rescue-dog--actions">
-              <a href={dog.imageUrl} class="glyphicon glyphicon-picture" title={`View the full size picture of ${dog.description}`} rel="noopener noreferrer" target="_blank">image</a>
+            <div className="rescue-dog--actions">
+              <a href={dog.imageUrl} title={`View the full size picture of ${dog.description}`} rel="noopener noreferrer" target="_blank">image</a>
             </div>
           </figure>
         </div>
